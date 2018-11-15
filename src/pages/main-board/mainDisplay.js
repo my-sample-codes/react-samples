520c7f5081888085e6bf4162a6776673ff2c97d2
Yashkumar Thakur <yashkumarthakur@gmail.com>
2018-11-15 18:09:01 +0530

    Working with db
    15/11/2018-- working module

--- src/pages/main-board/mainDisplay.js
+++ src/pages/main-board/mainDisplay.js
@@ -1,92 +1,59 @@
 import React, { Component } from 'react';
-import {Steps,Breadcrumb,Modal,Spin,Card, Col, Row, Button, Icon,Avatar} from 'antd';
+import { Steps, Breadcrumb, Modal, Spin, Card, Col, Row, Button, Icon, Avatar } from 'antd';
 import CollectionCreateForm from '../popup-form/collection'
-
+import axios from 'axios';
 export default class mainDisplay extends Component {
-    state = {visible: false,visible_spin: false,};
-      showModal = () => {this.setState({ visible: true });}
-      handleCancel = () => {this.setState({ visible: false });}
-    
-      handleCreate = () => {
-         const form = this.formRef.props.form;
-        form.validateFields((err, values) => {
-          if (err) {
-            return;
-          }
-          console.log('Received values of form: ', values);
-          form.resetFields();
-          this.setState({ visible: false });
-          this.setState({visible_spin:true});
-        });
+  state = { visible: false, loadervisible: false, };
+  // Performing a POST request
+  setVal(values) {
+    axios.post('http://10.10.18.20:8080/recon/product/save/',
+      {
+        productName: values.title,
+        productDescription: values.description,
+        productId: values.ProjectID,
+        documentType: values.document_type,
+        headerPresent: values.headerSet
+      }).then(function (response) {
+        console.log('saved successfully', response);
+        if (response.status == 200) {
+          setTimeout(() => this.setState({ loadervisible: false }), 800);
+          // this.setState({loadervisible: false});
+          console.log("in IF");
+        }
+      }.bind(this));
+  }
+  showModal = () => { this.setState({ visible: true }); }
+  handleCancel = () => { this.setState({ visible: false }); }
+  handleCancel1 = () => { this.setState({ loadervisible: false }); }
+  handleCreate = () => {
+    const form = this.formRef.props.form;
+    form.validateFields((err, values) => {
+      if (err) {
+        return;
       }
-      saveFormRef = (formRef) => {this.formRef = formRef;}
-      
-      render() {
-        return (
-          <div>
-            <Button type="primary" onClick={this.showModal}>Create New Project</Button>
-            <CollectionCreateForm
-              wrappedComponentRef={this.saveFormRef}
-              visible={this.state.visible}
-              onCancel={this.handleCancel}
-              onCreate={this.handleCreate}
-              visible_spin={this.state.visible_spin}
-            />
-            </div>  
-        );
-      }
-    }
-
-
-    // Basis
-    // state = { visible: false }
-
-    // showModal = () => {
-    //   this.setState({
-    //     visible: true,
-    //   });
-    // }
-  
-    // hideModal = () => {
-    //   this.setState({
-    //     visible: false,
-    //   });
-    // }
-
-    // render() {
-    //     return (
-    //       <div>
-    //         <Button type="primary" onClick={this.showModal}>Create New Project</Button>
-    //         <Modal
-    //           title="Modal"
-    //           visible={this.state.visible}
-    //           onOk={this.hideModal}
-    //           onCancel={this.hideModal}
-    //           okText="ok"
-    //           cancelText="cancel"
-    //         >
-    //           <p>Bla bla ...</p>
-    //           <p>Bla bla ...</p>
-    //           <p>Bla bla ...</p>
-    //         </Modal>
-    //       </div>
-    //     );
-    //   }
-
-
-      // Orignal
-    // render() {
-    //     return (
-    //         <div> 
-    //                 <center>
-    //                 <popupForNewProject />
-    //                     <Link to="/popupForm">
-    //                     <popupForm />
-    //                         <Button type='primary' onClick={}>Create New Project </Button>
-    //                     </Link>
-    //                 </center>
-    //         </div>
-
-    //     )   
-    // }
-
+      console.log('Received values of form: ', values);
+      this.setState({ visible: false });
+      this.setState({ loadervisible: true });
+      this.setVal(values);
+      form.resetFields();
+    });
+  }
+  saveFormRef = (formRef) => { this.formRef = formRef; }
+  render() {
+    return (
+      <div>
+        <Button type="primary" onClick={this.showModal}>Create New Project</Button>
+        <CollectionCreateForm
+          // Modal 1 for "New Project" Form
+          wrappedComponentRef={this.saveFormRef}
+          visible={this.state.visible}
+          onCancel={this.handleCancel}
+          onCreate={this.handleCreate}
+          //Modal 2 for Spinner
+          visible1={this.state.loadervisible}
+          onCancel1={this.handleCancel1}
+          onCreate2={this.handleCreate2} />
+      </div>
+    );
+  }
+}
