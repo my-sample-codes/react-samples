d32e9baefbcc4e52476bd4a0b05505261f6bcf59
kamleshkhamankar <42215909+kamleshkhamankar@users.noreply.github.com>
2018-11-20 16:07:39 +0530

    Update Login functionality
    directly call the LoginFrom.js file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-
-
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 import './App.css';
@@ -10,58 +8,15 @@ import 'antd/dist/antd.css';
 import './../src/pages/login/index.css';
 import './../src/pages/login/Login.css';
 
+import WrappedLoginForm from './pages/login/LoginForm';
+
 class App extends Component {
-
-     constructor(props)
-     {
-         super(props);
-
-         this.state={
-             username:'',
-             Password:'',
-             validated:false
-         }
-      this.signIn=this.signIn.bind(this);
-      this.onUserChange=this.onUserChange.bind(this);
-     }
-
-     signIn(){
-      
-        this.setState({validated:true});
-        console.log("in sign in",this.state.validated);
-        /*  if(this.state.username===null || this.state.Password==null)
-         {
-             alert("Crediential required");
-         }else if(this.state.username==="recon@opus.com" || this.state.Password=="1234"){
-           this.setState({validated:true});
-         }else
-         {  
-           alert("Username/Password Incorrect");
-         } */
-     }
-     onUserChange=(event,value)=>
-     {  console.log(value);
-        this.setState({username:value});
-     } 
     render() {
-        const isValidated=this.state.validated;
         return (
-             
-              <div className="Main" >
-                  {!isValidated?(<div>
-                      <img className='bg' src={BackgroundImg} />
-                      <Card title="Login" style={{ width: '350px', background: 'light grey', float: 'right' }} >
-                          <br />
-                          <p> <Input rows={1} placeholder="Enter Username" /></p><br /><br />
-                          <p> <Input rows={1} placeholder="Enter Password" /></p><br /><br />
-                          <center><Button type='primary' style={{ width: '200px' }} onClick={this.signIn}>Sign In</Button></center>
-                      </Card>
-                      </div>
-                  ):(<div><DashboardLand/></div>)}
-               
-                  
+            <div className="Main">
+              <WrappedLoginForm />
             </div>
-        );
+          );
     }
 }
 export default App;
