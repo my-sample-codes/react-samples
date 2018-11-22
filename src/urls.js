e00e97d2fb159997877b622b6030529d8a0729be
IshaBP <37435761+IshaBP@users.noreply.github.com>
2018-11-22 18:39:03 +0530

    Add files via upload

---
+++ src/urls.js
@@ -0,0 +1,19 @@
+
+export default {
+    urlarray: 
+        {           
+            "GetProducts": "http://10.11.14.79:8081/recon/product/getlist/",
+            "SaveProduct": "http://10.11.14.79:8081/recon/product/save/"
+        }
+
+    // urlarray: 
+    //     {           
+    //         "GetProducts": "/recon/product/getlist/",
+    //         "SaveProduct": "/recon/product/save/"
+    //     }
+
+    }
+
+
+    // urls.urlarray.GetProducts
+    // urls.urlarray.SaveProducts
