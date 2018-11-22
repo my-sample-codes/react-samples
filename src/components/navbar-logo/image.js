472134b67bc5f92dd70d767a0276ec478a2db8fb
vinuk2995 <35198687+vinuk2995@users.noreply.github.com>
2018-11-22 17:08:49 +0530

    Add files via upload
    logo size adjusted with header

--- src/components/navbar-logo/image.js
+++ src/components/navbar-logo/image.js
@@ -3,6 +3,7 @@ import 'antd/dist/antd.css';
 import React, { Component } from 'react';
 
 import tmobiles from './tmobiles.jpg';
+import tmobiles1 from './tmobiles1.png';
 
 import './image.css';
 
@@ -14,7 +15,7 @@ class Image extends Component {
     render() {
       return (
         <div className="logo">
-        <img src={tmobiles} alt="tmobiles" className="logo_img" />
+        <img src={tmobiles1} alt="tmobiles1" className="logo_img" />
       </div>
       );
     }
