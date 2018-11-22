f39f2a8a89fa82fa810e40d404b57f83d04601a0
pradipkakade <pradip.kakade@opusconsulting.com>
2018-11-22 20:52:27 +0530

    Login Integrated1

---
+++ src/pages/infomapper.js
@@ -0,0 +1,6 @@
+export default {
+    maparray: 
+        {           
+            "RecordExtract": "Here you can manage and manage the datasets you added.A dataset holds a raw data that can be used as a raw material without affecting your original data"
+        }
+    }
