56fad1b96bf432b8b93c413f3ddda94dabe143fb
pradipkakade <pradip.kakade@opusconsulting.com>
2018-11-23 17:10:50 +0530

    images added

---
+++ src/utility/urls.js
@@ -0,0 +1,20 @@
+
+export default {
+    urlarray: 
+        {           
+            "GetProducts": "http://10.11.14.79:8081/recon/product/getlist/",
+            "SaveProduct": "http://10.11.14.79:8081/recon/product/save/"
+        }
+
+    // urlarray: 
+    //     {           
+    //         "GetProducts": "/recon/product/getlist/",
+    //         "SaveProduct": "/recon/product/save/"
+    //     }
+
+    }
+
+    // var urls = require('./../../urls.js').default;
+
+    // urls.urlarray.GetProducts
+    // urls.urlarray.SaveProducts
